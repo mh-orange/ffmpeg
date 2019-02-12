6590c749eeb783337258d31f42423d71ef37e8da
Andrew Bates <abates@omeganetserv.com>
2019-02-12 10:36:45 -0500

    Documentation

--- input.go
+++ input.go
@@ -7,11 +7,17 @@ import (
 	"os"
 )
 
+// TranscoderInput is a TranscoderOption used to indicate how to access the input
+// media.  It could be a filename string, an io.ReadWriteSeeker or a URL.  The
+// TranscoderInput also has options for processing the input, such as seeking to
+// a start position or only processing a given duration
 type TranscoderInput interface {
 	TranscoderOption
 	input() *input
 }
 
+// InputOption is an option passed to a TranscoderInput to alter the input in some
+// way (such as specifying a starting position or duration)
 type InputOption func(*input) error
 
 type input struct {
@@ -58,10 +64,13 @@ func (in *input) process(job *transcodeJob) (err error) {
 	return
 }
 
+// Input creates a TranscoderInput and applies the options
 func Input(options ...InputOption) TranscoderInput {
 	return &input{options: options}
 }
 
+// InputFilename creates an InputOption that will pass the filename on
+// to the ffmpeg process
 func InputFilename(filename string) InputOption {
 	return func(input *input) (err error) {
 		input.fi, err = Stat(filename)
@@ -69,6 +78,8 @@ func InputFilename(filename string) InputOption {
 	}
 }
 
+// InputURL creates an InputOption that will pass the URL on to the
+// underlying ffmpeg process
 func InputURL(url *url.URL) InputOption {
 	return func(input *input) (err error) {
 		input.URL = url
@@ -76,6 +87,8 @@ func InputURL(url *url.URL) InputOption {
 	}
 }
 
+// InputFile will create an InputOption that reads the input file
+// and sends the data to the ffmpeg process using STDIN
 func InputFile(file *os.File) InputOption {
 	return func(input *input) (err error) {
 		input.fi, err = Stat(file.Name())
@@ -84,6 +97,9 @@ func InputFile(file *os.File) InputOption {
 	}
 }
 
+// StartOption sets the starting position that ffmpeg will attempt
+// to seek.  This sets the -ss option on the input on the ffmpeg command
+// line
 func StartOption(start Time) InputOption {
 	return func(input *input) error {
 		input.Start = start
@@ -91,6 +107,10 @@ func StartOption(start Time) InputOption {
 	}
 }
 
+// StartPercentOption can only be used if input media can be read by
+// the Stat function.  If the media can be read by the Stat function then
+// the -ss argument passed to ffmpeg will be a percentage of the input
+// streams duration
 func StartPercentOption(percent int) InputOption {
 	return func(input *input) error {
 		if input.fi == nil {
@@ -102,6 +122,8 @@ func StartPercentOption(percent int) InputOption {
 	}
 }
 
+// DurationOption will tell ffmpeg to process only "duration" frames
+// from the input file.  This uses the ffmpeg -t option
 func DurationOption(duration Time) InputOption {
 	return func(input *input) error {
 		input.Duration = duration
